403567212f8a1d95285b790f7f8b318d20e1a9d3
Paul Ahern <paul.ahern@digital.cabinet-office.gov.uk>
2021-06-25 11:00:12 +0100

    Correct capitalisation

--- accessibility_monitoring_platform/apps/websites/forms.py
+++ accessibility_monitoring_platform/apps/websites/forms.py
@@ -19,8 +19,8 @@ DEFAULT_SORT: str = "-website_id"
 SORT_CHOICES: List[Tuple[str, str]] = (
     (DEFAULT_SORT, "Most recently created"),
     # Todo: Add axe error count columns
-    # ("error_count", "Fewest Errors"),
-    # ("-error_count", "Most Errors"),
+    # ("error_count", "Fewest errors"),
+    # ("-error_count", "Most errors"),
     ("-last_updated", "Most recently updated"),
     ("last_updated", "First updated"),
 )
@@ -36,11 +36,11 @@ class WebsiteSearchForm(AMPDateRangeForm):
         label="Sector",
         queryset=Sector.objects.using("pubsecweb_db").all().order_by("sector_name"),
     )
-    location = AMPCharField(label="Town/City")
+    location = AMPCharField(label="Town/city")
     start_date = forms.DateField(
-        label="Last Updated Start date", widget=AMPDateWidget(), required=False
+        label="Last updated start date", widget=AMPDateWidget(), required=False
     )
     end_date = forms.DateField(
-        label="Last Updated End date", widget=AMPDateWidget(), required=False
+        label="Last updated end date", widget=AMPDateWidget(), required=False
     )
     sort_by = AMPChoiceField(choices=SORT_CHOICES)
