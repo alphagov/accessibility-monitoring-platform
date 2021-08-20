e7eb9073644dc8597bdb461145a32b6fdd9e6556
Paul Ahern <paul.ahern@digital.cabinet-office.gov.uk>
2021-08-20 16:37:21 +0100

    #277 Add watermark for test instance

--- accessibility_monitoring_platform/apps/cases/forms.py
+++ accessibility_monitoring_platform/apps/cases/forms.py
@@ -60,7 +60,7 @@ class CaseSearchForm(AMPDateRangeForm):
 
     sort_by = AMPChoiceField(label="Sort by", choices=SORT_CHOICES)
     search = AMPCharField(
-        label="Search", help_text="Searches in URLs and organisation names"
+        label="Search", help_text="Matches on URL, organisation, sector or location"
     )
     auditor = AMPChoiceField(label="Auditor")
     reviewer = AMPChoiceField(label="QA Auditor")
