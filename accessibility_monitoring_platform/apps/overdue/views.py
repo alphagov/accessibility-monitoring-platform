fe84e420c3fb09790ed3e89804155b87d3aa4167
richardbored <richard.board@digital.cabinet-office.gov.uk>
2022-01-27 15:15:50 +0000

    Fixed pagination

--- accessibility_monitoring_platform/apps/overdue/views.py
+++ accessibility_monitoring_platform/apps/overdue/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.db.models.query import QuerySet
+from ..cases.models import Case
 from .utils import get_overdue_cases
 
 
-class OverdueView(TemplateView):
+class OverdueView(ListView):
     """
     View of list of overdue tasks for user
     """
     template_name = "overdue/overdue_list.html"
+    models = Case
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Case]:
+        return get_overdue_cases(user_request=self.request.user)  # type: ignore
 
     def get_context_data(self, *args, **kwargs):
         context = super(OverdueView, self).get_context_data(*args, **kwargs)
-        in_correspondence = get_overdue_cases(user_request=self.request.user)  # type: ignore
-        if in_correspondence:
-            context["number_of_overdue"] = len(in_correspondence)
-        else:
-            context["number_of_overdue"] = 0
-        context["overdue_cases"] = in_correspondence
+        context["overdue_cases"] = get_overdue_cases(user_request=self.request.user)  # type: ignore
         return context
