db28e3d0eac1a0b5e218fabbac1947e399192ff8
Paul Ahern <ahernp@ahernp.com>
2022-04-26 16:16:45 +0100

    635: Warn when published report not the newest

--- report_viewer/apps/viewer/views.py
+++ report_viewer/apps/viewer/views.py
@@ -12,11 +12,11 @@ class ViewReport(TemplateView):
     """
 
     def get(self, request, guid, *args, **kwargs):  # pylint: disable=unused-argument
-        s3report = get_object_or_404(S3Report, guid=guid)
+        s3_report = get_object_or_404(S3Report, guid=guid)
         s3_rw = S3ReadWriteReport()
         raw_html = s3_rw.retrieve_raw_html_from_s3_by_guid(guid=guid)
-        report = Report.objects.get(case=s3report.case)
-        context = {"html_report": raw_html, "report": report}
+        report = Report.objects.get(case=s3_report.case)
+        context = {"html_report": raw_html, "report": report, "s3_report": s3_report}
         return render(
             request, "reports/accessibility_report_container.html", context=context
         )
