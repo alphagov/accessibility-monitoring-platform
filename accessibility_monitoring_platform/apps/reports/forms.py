9c979690b91acb0046a2c338205a16f044fcfb57
Paul Ahern <ahernp@ahernp.com>
2022-06-20 15:57:20 +0100

    752: Fewer capital letters; Remove report caption; Remove bold page names in table.

--- accessibility_monitoring_platform/apps/reports/forms.py
+++ accessibility_monitoring_platform/apps/reports/forms.py
@@ -96,7 +96,6 @@ class ReportWrapperUpdateForm(forms.ModelForm):
     """
 
     title = AMPCharFieldWide(label="Title")
-    title_caption = AMPCharFieldWide(label="Title caption")
     sent_by = AMPCharFieldWide(label="From (leave blank to hide)")
     contact = AMPCharFieldWide(label="Contact (leave blank to hide)")
     related_content = AMPTextField(label="Related content")
@@ -105,7 +104,6 @@ class ReportWrapperUpdateForm(forms.ModelForm):
         model = ReportWrapper
         fields: List[str] = [
             "title",
-            "title_caption",
             "sent_by",
             "contact",
             "related_content",
