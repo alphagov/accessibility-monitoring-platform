22ecb343a1b3a204431591624b3a8f65801b1471
Paul Ahern <ahernp@ahernp.com>
2022-06-24 15:54:37 +0100

    751: Reformat code with black.

--- accessibility_monitoring_platform/apps/users/tests/test_models.py
+++ accessibility_monitoring_platform/apps/users/tests/test_models.py
@@ -12,7 +12,5 @@ EMAIL_ADDRESS: str = "admin@email.com"
 def test_email_inclusion_list_returns_str():
     """Tests if AllowedEmail returns an email address"""
     AllowedEmail.objects.create(inclusion_email=EMAIL_ADDRESS)
-    email_list: AllowedEmail = AllowedEmail.objects.get(
-        inclusion_email=EMAIL_ADDRESS
-    )
+    email_list: AllowedEmail = AllowedEmail.objects.get(inclusion_email=EMAIL_ADDRESS)
     assert str(email_list) == EMAIL_ADDRESS
