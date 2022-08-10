b4fe8bab98dead15a79b72dc118cc2142ed881cf
Paul Ahern <ahernp@ahernp.com>
2022-08-10 13:21:22 +0100

    847: Restore tuples

--- accessibility_monitoring_platform/settings/base.py
+++ accessibility_monitoring_platform/settings/base.py
@@ -238,5 +238,5 @@ PERMISSIONS_POLICY = {
 CSP_DEFAULT_SRC = ("'none'",)
 CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 CSP_SCRIPT_SRC = ("'self'",)
-CSP_FONT_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'")
 CSP_IMG_SRC = ("'self'", "data:")
