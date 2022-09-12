5a36bd85e9e78417f5ca6c0e38c522b1fbfed742
Paul Ahern <ahernp@ahernp.com>
2022-09-12 15:02:22 +0100

    915: Add new case e2e test.

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -1,29 +1,17 @@
-/* global cy before Cypress */
+/* global cy */
 
 const username = 'auditor@email.com'
 const password = 'secret'
 
-describe('Login and visit pages', () => {
-  before(() => {
+describe('Login', () => {
+  it('can see dashboard page', () => {
     cy.visit('/account/login/')
     cy.get('input[name="auth-username"]').type(username)
     cy.get('input[name="auth-password"]').type(password)
-    cy.get('form').submit()
+    cy.get('form[data-cy="login-form"]').submit()
     cy.getCookie('sessionid').should('exist')
     cy.getCookie('csrftoken').should('exist')
-  })
-
-  beforeEach(() => {
-    Cypress.Cookies.preserveOnce('sessionid', 'csrftoken')
-  })
-
-  it('can see dashboard page', () => {
     cy.visit('/')
     cy.title().should('eq', 'Dashboard | Your cases')
-  })
-
-  it('can seee search page', () => {
-    cy.visit('/cases/')
-    cy.title().should('eq', 'Search')
   })
 })
--- cypress/cypress/support/commands.js
+++ cypress/cypress/support/commands.js
@@ -7,6 +7,6 @@ Cypress.Commands.add('login', () => {
   cy.visit('/account/login/')
   cy.get('input[name="auth-username"]').type(username)
   cy.get('input[name="auth-password"]').type(password)
-  cy.get('form').submit()
+  cy.get('form[data-cy="login-form"]').submit()
   cy.getCookie('sessionid').should('exist')
 })
