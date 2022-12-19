50159257849245df3b2a60f9df07c869ccd6c5ed
Paul Ahern <ahernp@ahernp.com>
2022-12-19 15:58:15 +0000

    1053: Add browser headers and admin options.

--- websites/admin.py
+++ websites/admin.py
@@ -1,16 +1,28 @@
 """Django admin configuration for websites app"""
 from django.contrib import admin
 
+from accessibility_monitoring_platform.apps.common.admin import ExportCsvMixin
 from .models import Website
 
 
-class WebsiteAdmin(admin.ModelAdmin):
+class WebsiteAdmin(admin.ModelAdmin, ExportCsvMixin):
     """Django admin configuration for Website model"""
 
-    list_display = ["url", "response_status_code", "response_type"]
+    list_display = ["url", "response_status_code", "response_type", "response_content"]
     list_filter = ["response_type", "response_status_code"]
-    readonly_fields = ["url", "response_status_code", "response_headers", "response_content"]
-    search_fields = ["url", "response_status_code", "response_headers", "response_content"]
+    readonly_fields = [
+        "url",
+        "response_status_code",
+        "response_headers",
+        "response_content",
+    ]
+    search_fields = [
+        "url",
+        "response_status_code",
+        "response_headers",
+        "response_content",
+    ]
+    actions = ["export_as_csv"]
 
 
 admin.site.register(Website, WebsiteAdmin)
--- websites/management/commands/populate_websites.py
+++ websites/management/commands/populate_websites.py
@@ -1,5 +1,7 @@
 """Populate websites"""
+# pylint: disable=line-too-long
 import requests
+from typing import Dict
 
 from django.core.management.base import BaseCommand
 from django.db.models import QuerySet
@@ -12,6 +14,10 @@ from ...models import (
     WEBSITE_RESPONSE_OTHER,
 )
 
+REQUEST_HEADERS: Dict[str, str] = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+}
+
 
 class Command(BaseCommand):
     """Django command to reset the websites data"""
@@ -21,10 +27,14 @@ class Command(BaseCommand):
         Website.objects.all().delete()
         cases: QuerySet[Case] = Case.objects.all()
         total_case: int = cases.count()
-        for count, case in enumerate(cases[100:], start=1):
+        for count, case in enumerate(cases, start=1):
             print(f"[{count}/{total_case}] {case}")
+            if case.home_page_url == "":
+                break
             try:
-                response: requests.Response = requests.get(case.home_page_url)
+                response: requests.Response = requests.get(
+                    case.home_page_url, headers=REQUEST_HEADERS, timeout=5
+                )
                 response_type: str = (
                     WEBSITE_RESPONSE_VALID
                     if response.status_code == 200
@@ -37,7 +47,7 @@ class Command(BaseCommand):
                     response_headers=str(response.headers),
                     response_content=response.content,
                 )
-            except Exception as exception:
+            except Exception as exception:  # pylint: disable=broad-except
                 Website.objects.create(
                     url=case.home_page_url,
                     response_type=WEBSITE_RESPONSE_ERROR,
