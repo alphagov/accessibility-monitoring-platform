5648dfc65fe0078879a1fb9a093ca60b9315d219
Paul Ahern <ahernp@ahernp.com>
2022-12-22 09:40:00 +0000

    1053: Tweak docker version.

--- websites/management/commands/run_axe_core.py
+++ websites/management/commands/run_axe_core.py
@@ -93,6 +93,12 @@ def run_axe_core():
     websites: QuerySet[Website] = Website.objects.all()
     chrome_options: Options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
     total_websites: int = websites.count()
     for count, website in enumerate(websites, start=1):
