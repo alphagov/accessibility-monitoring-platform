6c93eb1d866f9aa939d42f470f48c5a32e80b66b
Paul Ahern <ahernp@ahernp.com>
2023-01-31 08:53:19 +0000

    1089: Tidy code.

--- common/static/js/audits_check_filter.js
+++ common/static/js/audits_check_filter.js
@@ -4,17 +4,8 @@ All user to filter checks displayed by type, if check has been tested and WCAG d
 const testPage = document.getElementById('id_form-0-check_result_state_0') !== null
 const checkStateNameSuffix = testPage ? 'check_result_state' : 'retest_state'
 
-const divElements = document.getElementsByTagName('div')
-
-const checkListElements = Array.from(divElements).filter(divElement => divElement.hasAttribute('data-check-type'))
-if (testPage === true) {
-  checkListElements.forEach(checkListElement => {
-    checkListElement.addEventListener('input', updateNotTestedCounts)
-  })
-}
-
-const idName = document.querySelector('input[id="id_name"]')
-idName.addEventListener('input', updateCheckListFiltering)
+const textFilterInput = document.querySelector('input[id="id_name"]')
+textFilterInput.addEventListener('input', updateCheckListFiltering)
 
 const typeRadioButtons = document.querySelectorAll('input[name="type_filter"]')
 typeRadioButtons.forEach(typeRadioButton => {
@@ -28,11 +19,27 @@ stateRadioButtons.forEach(stateRadioButton => {
 
 document.getElementById('clear_search_form').addEventListener('click', resetFilterForm)
 
+const divElements = document.getElementsByTagName('div')
+const checkListElements = Array.from(divElements).filter(divElement => divElement.hasAttribute('data-check-type'))
+if (testPage === true) {
+  checkListElements.forEach(checkListElement => {
+    checkListElement.addEventListener('input', updateNotTestedCounts)
+  })
+}
+
+function resetFilterForm (event) {
+  event.preventDefault()
+  document.getElementById('id_name').value = ''
+  document.getElementById('id_type_filter_3').checked = true // Set type filter to all
+  document.getElementById('id_state_filter_3').checked = true // Set state filter to all
+  updateCheckListFiltering()
+}
+
 function updateCheckListFiltering (event) {
   const typeFilterValue = document.querySelector('input[name="type_filter"]:checked').value
   const stateFilterValue = document.querySelector('input[name="state_filter"]:checked').value
   const textFilterValue = document.getElementById('id_name').value
-  let errors = 0
+  let numberChecksDisplayed = 0
 
   checkListElements.forEach(checkListElement => {
     checkListElement.style.display = 'block'
@@ -47,12 +54,11 @@ function updateCheckListFiltering (event) {
       checkListElement.style.display = 'none'
     }
 
-    // Counts the number WCAG tests in list view
     if (checkListElement.style.display === 'block') {
-      errors += 1
+      numberChecksDisplayed += 1
     }
 
-    document.getElementById('number_of_errors').innerHTML = `Showing ${errors} errors`
+    document.getElementById('number_of_errors').innerHTML = `Showing ${numberChecksDisplayed} errors`
   })
 }
 
@@ -60,10 +66,10 @@ function updateNotTestedCounts (event) {
   let manualNotTested = 0
   let axeNotTested = 0
   let pdfNotTested = 0
+
   checkListElements.forEach(checkListElement => {
     const stateValue = checkListElement.querySelector('div.govuk-radios [name*="form-"][name*="-check_result_state"]:checked').value
     const typeValue = checkListElement.getAttribute("data-check-type")
-
     if (stateValue === 'not-tested') {
       switch (typeValue) {
         case 'manual':
@@ -81,14 +87,6 @@ function updateNotTestedCounts (event) {
   document.querySelector('label[for=id_type_filter_0]').innerHTML = `Manual tests (${manualNotTested} not tested)`
   document.querySelector('label[for=id_type_filter_1]').innerHTML = `Axe tests (${axeNotTested} not tested)`
   document.querySelector('label[for=id_type_filter_2]').innerHTML = `PDF (${pdfNotTested} not tested)`
-}
-
-function resetFilterForm (event) {
-  event.preventDefault()
-  document.getElementById('id_name').value = ''
-  document.getElementById('id_type_filter_3').checked = true // Set type filter to all
-  document.getElementById('id_state_filter_3').checked = true // Set state filter to all
-  updateCheckListFiltering()
 }
 
 updateCheckListFiltering()
