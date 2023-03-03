6ee9349ab666059bf4e3c72ebe76a3f3a908ef98
Paul Ahern <ahernp@ahernp.com>
2023-03-03 14:34:29 +0000

    1138: Tidy code

--- accessibility_monitoring_platform/apps/cases/utils.py
+++ accessibility_monitoring_platform/apps/cases/utils.py
@@ -470,15 +470,12 @@ def filter_cases(form: CaseSearchForm) -> QuerySet[Case]:  # noqa: C901
     if "reviewer_id" in filters and filters["reviewer_id"] == "none":
         filters["reviewer_id"] = None
 
-    if sort_by:
-        return (
-            Case.objects.filter(search_query, **filters)
-            .order_by(sort_by)
-            .select_related("auditor", "reviewer")
-            .all()
-        )
+    if not sort_by:
+        sort_by = "-id"
+
     return (
         Case.objects.filter(search_query, **filters)
+        .order_by(sort_by)
         .select_related("auditor", "reviewer")
         .all()
     )
