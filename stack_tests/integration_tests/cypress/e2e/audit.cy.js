f97624f3ff53981b59447311982c61283507da2c
Paul Ahern <ahernp@ahernp.com>
2023-03-24 14:18:38 +0000

    1147: Fix tests

--- stack_tests/integration_tests/cypress/e2e/audit.cy.js
+++ stack_tests/integration_tests/cypress/e2e/audit.cy.js
@@ -15,7 +15,7 @@ describe('View test', () => {
   })
 
   it('can edit test metadata', () => {
-    cy.contains('a', 'Edit test metadata').click()
+    cy.get('#edit-audit-metadata').click()
     cy.contains('Populate with today\'s date').click()
     cy.get('#id_screen_size').select('15 inch')
     cy.get('[name="exemptions_state"]').check('no')
@@ -28,14 +28,14 @@ describe('View test', () => {
   })
 
   it('can edit pages', () => {
-    cy.contains('a', 'Edit pages').click()
+    cy.get('#edit-audit-pages').click()
     cy.get('[name="standard-2-url"]').clear().type(accessibilityStatementURL)
     cy.get('[name="audit_pages_complete_date"]').click()
     cy.contains('Save').click()
   })
 
   it('can edit a page', () => {
-    cy.contains('a', 'Edit pages').click()
+    cy.get('#edit-audit-pages').click()
     cy.get('[data-cy="nav-step"]').contains('Home').click()
     cy.get('[name="form-0-check_result_state"]').check('error')
     cy.get('[name="form-0-notes"]').clear().type(errorText)
@@ -46,7 +46,7 @@ describe('View test', () => {
   })
 
   it('can edit website compliance decision', () => {
-    cy.contains('a', 'Edit website compliance decision').click()
+    cy.get('#edit-website-decision').click()
     cy.get('[name="case-is_website_compliant"]').check('partially-compliant')
     cy.get('[name="case-compliance_decision_notes"]').clear().type(websiteComplianceNote)
     cy.get('[name="audit_website_decision_complete_date"]').click()
@@ -56,7 +56,7 @@ describe('View test', () => {
   })
 
   it('can edit accessibility statement', () => {
-    cy.contains('a', 'Edit accessibility statement').click()
+    cy.get('#edit-audit-statement-1').click()
     cy.get('[name="accessibility_statement_backup_url"]').clear().type(accessibilityStatementBackupURL)
     cy.get('[name="scope_state"]').check('incomplete')
     cy.get('[name="audit_statement_1_complete_date"]').click()
@@ -69,7 +69,7 @@ describe('View test', () => {
   })
 
   it('can edit accessibility statement compliance decision', () => {
-    cy.contains('a', 'Edit accessibility statement compliance decision').click()
+    cy.get('#edit-statement-decision').click()
     cy.get('[name="case-accessibility_statement_state"]').check('not-compliant')
     cy.get('[name="case-accessibility_statement_notes"]').clear().type(accessibilityStatementComplianceNote)
     cy.get('[name="audit_statement_decision_complete_date"]').click()
@@ -79,7 +79,7 @@ describe('View test', () => {
   })
 
   it('can edit report options', () => {
-    cy.contains('a', 'Edit report options').click()
+    cy.get('#edit-audit-report-options').click()
     cy.get('[name="accessibility_statement_state"]').check('found-but')
     cy.get('[name="accessibility_statement_not_correct_format"]').click()
     cy.get('[name="report_next_change_statement"]').click()
@@ -91,7 +91,7 @@ describe('View test', () => {
   })
 
   it('can edit test summary', () => {
-    cy.contains('a', 'Edit report options').click()
+    cy.get('#edit-audit-report-options').click()
     cy.get('[data-cy="nav-step"]').contains('Test summary').click()
     cy.get('[name="audit_summary_complete_date"]').click()
     cy.contains('Save').click()
