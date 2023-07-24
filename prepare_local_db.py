f9ec7abfbc099a122b09c89cc282a4ce93e1a356
Richard Board <richard.board@digital.cabinet-office.gov.uk>
2023-07-24 14:37:11 +0100

    Updated prepare_local_db script

--- prepare_local_db.py
+++ prepare_local_db.py
@@ -1,3 +1,4 @@
+""" Prepare Local DB - Downloads the latest db backup and uploads to local postgres instance """
 from typing import Any, List
 import os
 from dotenv import load_dotenv
@@ -7,34 +8,21 @@ from pathlib import Path
 
 if __name__ == "__main__":
     load_dotenv()
-    s3_bucket: str = "paas-s3-broker-prod-lon-d9a58299-d162-49b5-8547-483663b17914"
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID_S3_STORE"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY_S3_STORE"),
-        region_name=os.getenv("AWS_DEFAULT_REGION_S3_STORE"),
-    )
+    s3_bucket: str = "amp-aurora-backup-prod"
+    s3_client = boto3.client("s3")
 
     file_name: str = ""
     s3_path: str = ""
-    if os.getenv("DB_BACKUP"):
-        # Gets specific db backup if DB_BACKUP has S3 path
-        if isinstance(os.getenv("DB_BACKUP"), str):
-            s3_path = str(os.getenv("DB_BACKUP"))
-            filename = s3_path.rsplit("/", maxsplit=1)[-1]
-    else:
-        # Else it justs get the latest production backup
-        db_backups: List[Any] = []
-        for key in s3_client.list_objects(Bucket=s3_bucket)["Contents"]:
-            if (
-                "deploy_feature_to_paas_db_back/" in key["Key"]
-                and "production" in key["Key"]
-            ):
-                db_backups.append(key)
-        db_backups.sort(key=lambda x: x["LastModified"])
-        file_name = db_backups[-1]["Key"].split("/")[-1]
-        s3_path = db_backups[-1]["Key"]
-
+    db_backups: List[Any] = []
+    for key in s3_client.list_objects(Bucket=s3_bucket)["Contents"]:
+        if (
+            "aws_aurora_backup/" in key["Key"]
+            and "prodenv" in key["Key"]
+        ):
+            db_backups.append(key)
+    db_backups.sort(key=lambda x: x["LastModified"])
+    file_name = db_backups[-1]["Key"].split("/")[-1]
+    s3_path = db_backups[-1]["Key"]
     Path("./data/s3_files/").mkdir(parents=True, exist_ok=True)
     local_path: str = f"./data/s3_files/{file_name}"
 
