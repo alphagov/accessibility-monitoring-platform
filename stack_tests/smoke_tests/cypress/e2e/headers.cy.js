25866282866c71f82dea5786108e2a6009e24141
Paul Ahern <ahernp@ahernp.com>
2023-08-22 15:10:39 +0100

    1337: Add tests

--- stack_tests/smoke_tests/cypress/e2e/headers.cy.js
+++ stack_tests/smoke_tests/cypress/e2e/headers.cy.js
@@ -1,7 +1,23 @@
 
 /* global cy, Cypress */
 
-describe('Platform production Response headers', () => {
+describe('Platform staging response headers', () => {
+  it('include strict transport security', () => {
+    cy.request('/').then((response) => {
+      expect(response).to.have.property('headers')
+      expect(response.headers).to.have.property('strict-transport-security', 'max-age=2592000; includeSubDomains; preload')
+    })
+  })
+})
+describe('Platform test response headers', () => {
+  it('include strict transport security', () => {
+    cy.request('https://platform-test.accessibility-monitoring.service.gov.uk/').then((response) => {
+      expect(response).to.have.property('headers')
+      expect(response.headers).to.have.property('strict-transport-security', 'max-age=2592000; includeSubDomains; preload')
+    })
+  })
+})
+describe('Platform production response headers', () => {
   it('include strict transport security', () => {
     cy.request('https://platform.accessibility-monitoring.service.gov.uk/').then((response) => {
       expect(response).to.have.property('headers')
