61644180f2e8ad008604465a1260a941c69e4463
Paul Ahern <ahernp@ahernp.com>
2024-03-15 15:06:36 +0000

    1556: Consolidate comments, notifications and reminders

--- accessibility_monitoring_platform/apps/comments/admin.py
+++ accessibility_monitoring_platform/apps/comments/admin.py
@@ -1,7 +1,8 @@
 """Admin - admin page for comments"""
+
 from django.contrib import admin
 
-from .models import Comment
+from .models import Alert, Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
@@ -22,4 +23,33 @@ class CommentAdmin(admin.ModelAdmin):
     ]
 
 
+class AlertAdmin(admin.ModelAdmin):
+    """Django admin configuration for Alert model"""
+
+    readonly_fields = ["created", "comment"]
+    search_fields = [
+        "case__organisation_name",
+        "case__id",
+        "target_user__username",
+        "message",
+    ]
+    list_display = [
+        "case",
+        "target_user",
+        "type",
+        "due_date",
+        "message",
+        "read",
+        "is_deleted",
+    ]
+    list_filter = [
+        "type",
+        "read",
+        "is_deleted",
+        ("target_user", admin.RelatedOnlyFieldListFilter),
+    ]
+    show_facets = admin.ShowFacets.ALWAYS
+
+
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(Alert, AlertAdmin)
--- accessibility_monitoring_platform/apps/comments/models.py
+++ accessibility_monitoring_platform/apps/comments/models.py
@@ -1,8 +1,10 @@
-"""Models for comment and comment history"""
-from typing import List
+"""Models for comments and user-specific alerts"""
+
+from typing import Dict, List
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 from accessibility_monitoring_platform.apps.cases.models import Case
@@ -11,6 +13,15 @@ from accessibility_monitoring_platform.apps.cases.models import Case
 class Comment(models.Model):
     """Comment model"""
 
+    class Type(models.TextChoices):
+        QA = "qa", "QA"
+        EXPORT = "export", "Equality body export"
+
+    type = models.CharField(
+        max_length=20,
+        choices=Type.choices,
+        default=Type.QA,
+    )
     case = models.ForeignKey(
         Case,
         on_delete=models.PROTECT,
@@ -39,3 +50,54 @@ class Comment(models.Model):
     def save(self, *args, **kwargs) -> None:
         self.updated = timezone.now()
         super().save(*args, **kwargs)
+
+
+class Alert(models.Model):
+    """Alert for a specific user model"""
+
+    class Type(models.TextChoices):
+        QA_COMMENT = "qa-comment", "QA comment"
+        REPORT_APPROVED = "report-approved", "Report approved"
+        REMINDER = "reminder", "Reminder"
+
+    type = models.CharField(
+        max_length=20,
+        choices=Type.choices,
+        default=Type.QA_COMMENT,
+    )
+    target_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    case = models.ForeignKey(Case, on_delete=models.PROTECT)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.PROTECT, blank=True, null=True
+    )
+    message = models.TextField(default="")
+    read = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    due_date = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self) -> str:
+        return str(f"{self.get_type_display()} | #{self.message}")
+
+    def get_absolute_url(self):
+        return reverse("cases:case-detail", kwargs={"pk": self.case.id})
+
+    @property
+    def target_url(self) -> str:
+        case_pk_id: Dict[str, int] = {"pk": self.case.id}
+        if self.type == Alert.Type.REPORT_APPROVED:
+            return reverse("cases:edit-report-approved", kwargs=case_pk_id)
+        return reverse("cases:edit-qa-comments", kwargs=case_pk_id)
+
+    @property
+    def target_label(self) -> str:
+        label_prefix: str = f"{self.case} - "
+        if self.type == Alert.Type.REPORT_APPROVED:
+            label_prefix += "Report approved"
+        else:
+            label_prefix += "COMMENT"
+        return label_prefix
