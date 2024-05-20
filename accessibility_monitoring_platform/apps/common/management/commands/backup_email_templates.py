f6e0d95deeffd6e16f19e5052a6f72d171416a39
Paul Ahern <ahernp@ahernp.com>
2024-05-20 09:03:52 +0100

    1662: Add command to backup email templates from database to codebase

---
+++ accessibility_monitoring_platform/apps/common/management/commands/backup_email_templates.py
@@ -0,0 +1,22 @@
+"""Backup email templates from database to code base"""
+
+from django.core.management.base import BaseCommand
+
+from ...models import EmailTemplate
+
+PATH_TO_EMAILS_DIRECTORY: str = (
+    "accessibility_monitoring_platform/apps/common/templates/common/emails/backup/"
+)
+
+
+class Command(BaseCommand):
+    """Django command to backup email templates"""
+
+    def handle(self, *args, **options):  # pylint: disable=unused-argument
+        """Read all email templates from database and save as files"""
+        for email_template in EmailTemplate.objects.all():
+            backup_file = open(
+                f"{PATH_TO_EMAILS_DIRECTORY}{email_template.slug}.html", "w"
+            )
+            backup_file.write(str(email_template.template))
+            backup_file.close()
