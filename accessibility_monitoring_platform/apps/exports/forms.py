d61deccdbd49aa472278e77c82b6e84b0112bd93
Paul Ahern <ahernp@ahernp.com>
2024-05-21 15:49:43 +0100

    1610: Add ECNI export manager

--- accessibility_monitoring_platform/apps/exports/forms.py
+++ accessibility_monitoring_platform/apps/exports/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
+from ..cases.models import Case, CaseStatus
 from ..common.forms import AMPBooleanCheckboxWidget, AMPDateField
 from .models import Export
 
@@ -22,6 +23,16 @@ class ExportCreateForm(forms.ModelForm):
         cutoff_date = self.cleaned_data["cutoff_date"]
         if Export.objects.filter(cutoff_date=cutoff_date).exists():
             raise ValidationError("Export for this date already exists")
+        if (
+            CaseStatus.objects.filter(
+                status=CaseStatus.Status.CASE_CLOSED_WAITING_TO_SEND
+            )
+            .filter(case__enforcement_body=self.instance.enforcement_body)
+            .filter(case__compliance_email_sent_date__lte=cutoff_date)
+            .exclude(case__case_completed=Case.CaseCompleted.COMPLETE_NO_SEND)
+            .exists()
+        ):
+            raise ValidationError("There are no cases to export")
         return cutoff_date
 
 
