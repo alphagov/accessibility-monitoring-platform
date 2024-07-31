8834bc30f9d9e9e5ccda2336326438775dbb4e36
Paul Ahern <ahernp@ahernp.com>
2024-07-31 10:59:21 +0100

    1744: Show Case nav sections on PSB Zendesk pages

--- accessibility_monitoring_platform/apps/cases/views.py
+++ accessibility_monitoring_platform/apps/cases/views.py
@@ -1260,7 +1260,7 @@ class CaseLegacyEndOfCaseUpdateView(CaseUpdateView):
     template_name: str = "cases/forms/legacy_end_of_case.html"
 
 
-class CaseZendeskTicketsDetailView(DetailView):
+class CaseZendeskTicketsDetailView(CaseNavContextMixin, DetailView):
     """
     View of Zendesk tickets for a case
     """
@@ -1277,7 +1277,7 @@ class CaseZendeskTicketsDetailView(DetailView):
         return context
 
 
-class ZendeskTicketCreateView(CreateView):
+class ZendeskTicketCreateView(CaseNavContextMixin, CreateView):
     """
     View to create a Zendesk ticket
     """
@@ -1310,7 +1310,7 @@ class ZendeskTicketCreateView(CreateView):
         return reverse("cases:zendesk-tickets", kwargs=case_pk)
 
 
-class ZendeskTicketUpdateView(UpdateView):
+class ZendeskTicketUpdateView(CaseNavContextMixin, UpdateView):
     """
     View to update Zendesk ticket
     """
