3b0d60fa3435bc8c9f3e9461bc66f8c7e6af3043
Paul Ahern <ahernp@ahernp.com>
2024-10-10 16:26:35 +0100

    1784: Fix integration tests

--- stack_tests/integration_tests/cypress/e2e/audit.cy.js
+++ stack_tests/integration_tests/cypress/e2e/audit.cy.js
@@ -14,7 +14,8 @@ describe('View test', () => {
   })
 
   it('can edit test metadata', () => {
-    cy.get('#edit-audit-metadata').click()
+    cy.contains('Initial WCAG test').click()
+    cy.get('#edit-audits1edit-audit-metadata').click()
     cy.contains('Populate with today\'s date').click()
     cy.get('#id_screen_size').select('15 inch')
     cy.get('[name="exemptions_state"]').check('no')
@@ -26,15 +27,17 @@ describe('View test', () => {
   })
 
   it('can edit pages', () => {
-    cy.get('#edit-audit-pages').click()
+    cy.contains('Initial WCAG test').click()
+    cy.get('#edit-audits1edit-audit-pages').click()
     cy.get('[name="standard-4-url"]').clear().type(accessibilityStatementURL)
     cy.get('[name="audit_pages_complete_date"]').click()
     cy.contains('Save').click()
   })
 
   it('can edit a page', () => {
-    cy.get('#edit-audit-pages').click()
-    cy.contains(/^Home page test$/).click()
+    cy.contains('Initial WCAG test').click()
+    cy.get('#edit-audits1edit-audit-pages').click()
+    cy.contains(/^Home page test \(1\)$/).click()
     cy.get('[name="form-0-check_result_state"]').check('error')
     cy.get('[name="form-0-notes"]').clear().type(errorText)
     cy.get('[name="complete_date"]').click()
@@ -44,7 +47,8 @@ describe('View test', () => {
   })
 
   it('can edit website compliance decision', () => {
-    cy.get('#edit-website-decision').click()
+    cy.contains('Initial WCAG test').click()
+    cy.get('#edit-audits1edit-website-decision').click()
     cy.get('[name="case-compliance-website_compliance_state_initial"]').check('partially-compliant')
     cy.get('[name="case-compliance-website_compliance_notes_initial"]').clear().type(websiteComplianceNote)
     cy.get('[name="audit_website_decision_complete_date"]').click()
@@ -53,8 +57,16 @@ describe('View test', () => {
     cy.contains(websiteComplianceNote)
   })
 
+  it('can edit WCAG test summary', () => {
+    cy.contains('Initial WCAG test').click()
+    cy.get('#edit-audits1edit-audit-wcag-summary').click()
+    cy.get('[name="audit_wcag_summary_complete_date"]').click()
+    cy.contains('Save').click()
+  })
+
   it('can edit accessibility statement', () => {
-    cy.get('#edit-audit-statement-1').click()
+    cy.contains('Initial statement').click()
+    cy.get('#edit-audits1edit-audit-statement-one').click()
     cy.get('[name="archive_scope_state"]').check('incomplete')
     cy.get('[name="archive_audit_statement_1_complete_date"]').click()
     cy.contains('Save and continue').click()
@@ -64,7 +76,8 @@ describe('View test', () => {
   })
 
   it('can edit accessibility statement compliance decision', () => {
-    cy.get('#edit-statement-decision').click()
+    cy.contains('Initial statement').click()
+    cy.get('#edit-audits1edit-statement-decision').click()
     cy.get('[name="case-compliance-statement_compliance_state_initial"]').check('not-compliant')
     cy.get('[name="case-compliance-statement_compliance_notes_initial"]').clear().type(accessibilityStatementComplianceNote)
     cy.get('[name="audit_statement_decision_complete_date"]').click()
@@ -73,21 +86,10 @@ describe('View test', () => {
     cy.contains(accessibilityStatementComplianceNote)
   })
 
-  it('can edit report options', () => {
-    cy.get('#edit-audit-report-options').click()
-    cy.get('[name="archive_accessibility_statement_state"]').check('found-but')
-    cy.get('[name="archive_accessibility_statement_not_correct_format"]').click()
-    cy.get('[name="archive_report_next_change_statement"]').click()
-    cy.get('[name="archive_report_options_notes"]').clear().type(reportOptionsNote)
-    cy.get('[name="archive_audit_report_options_complete_date"]').click()
-    cy.contains('Save').click()
-    cy.visit('/cases/1/view')
-    cy.contains(reportOptionsNote)
-  })
-
-  it('can edit test summary', () => {
-    cy.get('#edit-audit-wcag-summary').click()
-    cy.get('[name="audit_wcag_summary_complete_date"]').click()
+  it('can edit statement test summary', () => {
+    cy.contains('Initial statement').click()
+    cy.get('#edit-audits1edit-audit-statement-summary').click()
+    cy.get('[name="audit_statement_summary_complete_date"]').click()
     cy.contains('Save').click()
   })
 })
