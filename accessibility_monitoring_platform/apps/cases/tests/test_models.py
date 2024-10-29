8de7e3b72489c4cb9dc1999c2d0db3495ef83aef
Paul Ahern <ahernp@ahernp.com>
2024-10-29 14:08:01 +0000

    1813: Remove unused constants

--- accessibility_monitoring_platform/apps/cases/tests/test_models.py
+++ accessibility_monitoring_platform/apps/cases/tests/test_models.py
@@ -61,19 +61,12 @@ NO_CONTACT_DATE: date = date(2020, 4, 1)
 NO_CONTACT_ONE_WEEK: date = NO_CONTACT_DATE + timedelta(days=7)
 NO_CONTACT_FOUR_WEEKS: date = NO_CONTACT_DATE + timedelta(days=28)
 TODAY: date = date.today()
-YESTERDAY = TODAY - timedelta(days=1)
 ONE_WEEK_AGO = TODAY - timedelta(days=7)
 TWO_WEEKS_AGO = TODAY - timedelta(days=14)
-THREE_WEEKS_AGO = TODAY - timedelta(days=21)
 FOUR_WEEKS_AGO = TODAY - timedelta(days=28)
-FIVE_WEEKS_AGO = TODAY - timedelta(days=35)
-ELEVEN_WEEKS_AGO = TODAY - timedelta(days=77)
-TWELVE_WEEKS_AGO = TODAY - timedelta(days=84)
-THIRTEEN_WEEKS_AGO = TODAY - timedelta(days=91)
-FOURTEEN_WEEKS_AGO = TODAY - timedelta(days=98)
 
 
-def create_case() -> Case:
+def create_case_for_overdue_link() -> Case:
     user: User = User.objects.create()
     case: Case = create_case_and_compliance(
         created=datetime.now().tzinfo,
@@ -82,12 +75,9 @@ def create_case() -> Case:
         auditor=user,
         website_compliance_state_initial=CaseCompliance.WebsiteCompliance.COMPLIANT,
         statement_compliance_state_initial=CaseCompliance.StatementCompliance.COMPLIANT,
-        report_draft_url="https://www.report-draft.com",
         report_review_status=Boolean.YES,
         reviewer=user,
         report_approved_status=Case.ReportApprovedStatus.APPROVED,
-        report_final_pdf_url="https://www.report-pdf.com",
-        report_final_odt_url="https://www.report-odt.com",
     )
     return case
 
@@ -1514,7 +1504,7 @@ def test_overdue_link_seven_day_no_contact():
     contact email sent date is more than seven days ago and no
     chaser emails have been sent.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.contact_details_found = Case.ContactDetailsFound.NOT_FOUND
     case.seven_day_no_contact_email_sent_date = ONE_WEEK_AGO
@@ -1534,7 +1524,7 @@ def test_overdue_link_no_contact_one_week_chaser():
     contact email sent date is more than seven days ago and only the
     one week chaser email has been sent.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.contact_details_found = Case.ContactDetailsFound.NOT_FOUND
     case.seven_day_no_contact_email_sent_date = TWO_WEEKS_AGO
@@ -1555,7 +1545,7 @@ def test_overdue_link_no_contact_four_week_chaser():
     contact email sent date is more than seven days ago and only the
     four week chaser email has been sent.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.contact_details_found = Case.ContactDetailsFound.NOT_FOUND
     case.seven_day_no_contact_email_sent_date = TWO_WEEKS_AGO
@@ -1575,7 +1565,7 @@ def test_overdue_link_report_one_week_followup():
     Check overdue link if case is in report correspondence,
     the one week followup is due but not sent.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.report_followup_week_1_due_date = ONE_WEEK_AGO
     case.save()
@@ -1594,7 +1584,7 @@ def test_overdue_link_report_four_week_followup():
     Check overdue link if case is in report correspondence,
     the four week followup is due but not sent.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.report_followup_week_4_due_date = ONE_WEEK_AGO
     case.report_followup_week_1_sent_date = ONE_WEEK_AGO
@@ -1614,7 +1604,7 @@ def test_overdue_link_report_four_week_followup_sent_one_week_ago():
     Check overdue link if case is in report correspondence,
     the four week followup was sent over a week ago.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.report_followup_week_4_due_date = FOUR_WEEKS_AGO
     case.report_followup_week_1_sent_date = FOUR_WEEKS_AGO
@@ -1638,7 +1628,7 @@ def test_overdue_link_12_week_deadline_due():
     Check overdue link if case is awaiting 12-week deadline,
     the 12-week due date has passed.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.report_followup_week_12_due_date = ONE_WEEK_AGO
     case.save()
@@ -1657,7 +1647,7 @@ def test_overdue_link_12_week_correspondence_1_week_chaser_due():
     Check overdue link if case is in 12-week correspondence,
     the 1-week chaser due date has passed.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.twelve_week_update_requested_date = TWO_WEEKS_AGO
     case.twelve_week_1_week_chaser_due_date = ONE_WEEK_AGO
@@ -1677,7 +1667,7 @@ def test_overdue_link_12_week_correspondence_1_week_chaser_sent_a_week_ago():
     Check overdue link if case is in 12-week correspondence,
     the 1-week chaser was sent a week ago.
     """
-    case: Case = create_case()
+    case: Case = create_case_for_overdue_link()
 
     case.twelve_week_update_requested_date = TWO_WEEKS_AGO
     case.twelve_week_1_week_chaser_due_date = ONE_WEEK_AGO
