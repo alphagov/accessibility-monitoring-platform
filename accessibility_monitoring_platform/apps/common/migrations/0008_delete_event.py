e2c87cecd276170a195cbd88915058d6eeb159fe
Paul Ahern <ahernp@ahernp.com>
2025-06-10 14:53:22 +0100

    1974: Add another missing migration dependency

--- accessibility_monitoring_platform/apps/common/migrations/0008_delete_event.py
+++ accessibility_monitoring_platform/apps/common/migrations/0008_delete_event.py
@@ -6,6 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ("cases", "0010_populate_events"),
         ("common", "0007_populate_template_name"),
     ]
 
