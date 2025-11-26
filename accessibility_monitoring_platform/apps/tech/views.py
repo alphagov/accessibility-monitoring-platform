489dc39fa60a50a836bc4344c4c28e5a62179cb5
Paul Ahern <ahernp@ahernp.com>
2025-11-26 16:27:14 +0000

    2157: Add page showing equality body CSV metadata

--- accessibility_monitoring_platform/apps/tech/views.py
+++ accessibility_monitoring_platform/apps/tech/views.py
@@ -11,9 +11,27 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
 from ..common.models import Boolean
+from ..detailed.csv_export import (
+    DETAILED_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+    DETAILED_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+    DETAILED_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+    DETAILED_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+)
 from ..detailed.models import DetailedCase
+from ..mobile.csv_export import (
+    MOBILE_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+    MOBILE_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+    MOBILE_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+    MOBILE_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+)
 from ..mobile.models import MobileCase
 from ..reports.models import Report
+from ..simplified.csv_export import (
+    SIMPLIFIED_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+    SIMPLIFIED_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+    SIMPLIFIED_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+    SIMPLIFIED_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+)
 from ..simplified.models import SimplifiedCase
 from .forms import ImportCSVForm, ImportTrelloCommentsForm, PlatformCheckingForm
 from .utils import import_mobile_cases_csv, import_trello_comments
@@ -24,26 +42,6 @@ logger = logging.getLogger(__name__)
 class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
         return self.request.user.is_staff
-
-
-class PlatformCheckingView(StaffRequiredMixin, FormView):
-    """Write log message"""
-
-    form_class = PlatformCheckingForm
-    template_name: str = "tech/platform_checking.html"
-    success_url: str = reverse_lazy("tech:platform-checking")
-
-    def form_valid(self, form):
-        if "trigger_400" in self.request.POST:
-            raise BadRequest
-        if "trigger_403" in self.request.POST:
-            raise PermissionDenied
-        if "trigger_500" in self.request.POST:
-            1 / 0
-        logger.log(
-            level=int(form.cleaned_data["level"]), msg=form.cleaned_data["message"]
-        )
-        return super().form_valid(form)
 
 
 class ReferenceImplementaionView(StaffRequiredMixin, TemplateView):
@@ -72,6 +70,60 @@ class ReferenceImplementaionView(StaffRequiredMixin, TemplateView):
             archive=""
         ).first()
         return context
+
+
+class EqualityBodyCsvMetadataView(StaffRequiredMixin, TemplateView):
+    """
+    View showing euality body CSV metadata for simplified, detailed and mobile cases
+    and highlighting differences.
+    """
+
+    template_name: str = "tech/equality_body_csv_metadata.html"
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        """Get context data for template rendering"""
+        context: dict[str, Any] = super().get_context_data(**kwargs)
+        context["metadata"] = zip(
+            SIMPLIFIED_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+            DETAILED_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+            MOBILE_EQUALITY_BODY_METADATA_COLUMNS_FOR_EXPORT,
+        )
+        context["report"] = zip(
+            SIMPLIFIED_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+            DETAILED_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+            MOBILE_EQUALITY_BODY_REPORT_COLUMNS_FOR_EXPORT,
+        )
+        context["correspondence"] = zip(
+            SIMPLIFIED_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+            DETAILED_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+            MOBILE_EQUALITY_BODY_CORRESPONDENCE_COLUMNS_FOR_EXPORT,
+        )
+        context["test_summary"] = zip(
+            SIMPLIFIED_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+            DETAILED_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+            MOBILE_EQUALITY_BODY_TEST_SUMMARY_COLUMNS_FOR_EXPORT,
+        )
+        return context
+
+
+class PlatformCheckingView(StaffRequiredMixin, FormView):
+    """Write log message"""
+
+    form_class = PlatformCheckingForm
+    template_name: str = "tech/platform_checking.html"
+    success_url: str = reverse_lazy("tech:platform-checking")
+
+    def form_valid(self, form):
+        if "trigger_400" in self.request.POST:
+            raise BadRequest
+        if "trigger_403" in self.request.POST:
+            raise PermissionDenied
+        if "trigger_500" in self.request.POST:
+            1 / 0
+        logger.log(
+            level=int(form.cleaned_data["level"]), msg=form.cleaned_data["message"]
+        )
+        return super().form_valid(form)
 
 
 class ImportCSV(StaffRequiredMixin, FormView):
