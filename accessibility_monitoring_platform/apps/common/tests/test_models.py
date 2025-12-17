c6ef95b83a9193beac9071d63973e70bc1db2218
Paul Ahern <ahernp@ahernp.com>
2025-12-17 15:32:14 +0000

    2098: Add test

--- accessibility_monitoring_platform/apps/common/tests/test_models.py
+++ accessibility_monitoring_platform/apps/common/tests/test_models.py
@@ -2,7 +2,7 @@
 Tests for common models
 """
 
-from ..models import IssueReport
+from ..models import EmailTemplate, IssueReport
 
 
 def test_issue_number_incremented_on_creation(admin_user):
@@ -14,3 +14,11 @@ def test_issue_number_incremented_on_creation(admin_user):
     issue_report_two: IssueReport = IssueReport.objects.create(created_by=admin_user)
 
     assert issue_report_two.issue_number == 2
+
+
+def test_template_path():
+    """Test EmailTemplate.template_path"""
+    assert (
+        EmailTemplate(template_name="template-name").template_path
+        == "common/emails/templates/template-name.html"
+    )
