46dd39a7c668a2f6e1d4633d8c2559a4a4233106
Paul Ahern <ahernp@ahernp.com>
2026-06-05 08:54:32 +0100

    2223: Check platform gets security.txt from vdp.cabinetoffice.gov.uk

--- test_urls.py
+++ test_urls.py
@@ -2,9 +2,10 @@
 Test top-level urls
 """
 
+from unittest.mock import MagicMock, patch
+
 import pytest
 from django.http.response import HttpResponse
-from pytest_django.asserts import assertContains
 
 
 def test_robots_txt(client):
@@ -19,9 +20,15 @@ def test_robots_txt(client):
     "url",
     ["/security.txt", "/.well-known/security.txt"],
 )
-def test_security_txt(url, client):
-    """Test security txt"""
-    response: HttpResponse = client.get(url)
+@patch("accessibility_monitoring_platform.urls.requests")
+def test_security_txt(mock_requests, url, client):
+    """Test security txt gotten from vdp.cabinetoffice.gov.u"""
+    mock_requests_response: MagicMock = MagicMock()
+    mock_requests_response.status_code = 200
+    mock_requests.get.return_value = mock_requests_response
 
-    assert response.status_code == 200
-    assertContains(response, "Policy: https://www.gov.uk/help/report-vulnerability")
+    client.get(url)
+
+    mock_requests.get.assert_called_once_with(
+        "https://vdp.cabinetoffice.gov.uk/.well-known/security.txt", stream=True
+    )
