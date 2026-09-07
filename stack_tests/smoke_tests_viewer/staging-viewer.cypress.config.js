632f11c3718b664faeb21a5d8876a812256af4eb
Richard Board <RBoard@MAC-Dev-HY9GQ2P9LR.local>
2026-09-07 14:20:58 +0100

    Fixed report staging url

--- stack_tests/smoke_tests_viewer/staging-viewer.cypress.config.js
+++ stack_tests/smoke_tests_viewer/staging-viewer.cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    baseUrl: 'https://reports-stage.accessibility-monitoring.service.gov.uk',
+    baseUrl: 'https://reports-staging.accessibility-monitoring.service.gov.uk',
     chromeWebSecurity: false,
     supportFile: false
   }
